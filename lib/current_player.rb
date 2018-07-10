146fcce376f7a894eecba61191afcc29f6952cf9
Lloyd Lee <kinglloydlee@gmail.com>
2018-07-10 21:07:29 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,18 @@
 def turn_counter(board)
   counter = 0
   board.each do |taken|
-    if taken == "X" 
+    if taken == "X" || taken == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_counter%2 == 0
+    return "X"
+    
+  elsif turn_counter(board)%2 == 1
+    return "O"
+  end
+end
