583d52eef0c4c34b21dca9bec160d19a36d03cb6
Lloyd Lee <kinglloydlee@gmail.com>
2018-07-10 21:11:06 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,10 +9,8 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count%2 == 0
+  if turn_count(board) % 2 == 0
     return "X"
-    
-  elsif turn_count(board)%2 == 1
-    return "O"
-  end
+  else return "O"
+end
 end
